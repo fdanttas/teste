43ec6d17cb1a8b84b73639db6640eedf99e45c1a
Danttas <164497918+fdanttas@users.noreply.github.com>
2025-02-17 18:43:12 +0000

    Salvando

---
+++ 2. tipagem_estatica.py
@@ -0,0 +1,15 @@
+import os
+os.system("clear")
+# Definçãode tipos obrigatoria
+
+nome:str # equivalente ao caracter
+idade:str # Equivalente ao inteiro
+altura:float # equivalente ao tipo real
+
+nome = "Marta"
+idade = 23
+altura = 1.65
+
+print(nome)
+print(idade)
+print(altura)
